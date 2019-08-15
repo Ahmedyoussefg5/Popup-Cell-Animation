688d4d8cc5222b0279315bc73ebf71b65fac75f3
Ahmed Youssef <ahmed.youssef@itgeeks.info>
2019-08-15 18:30:27 +0200

    Initial Commit

---
+++ Popup Cell Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Popup Cell Animation
+//
+//  Created by Youssef on 8/15/19.
+//  Copyright © 2019 Youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
